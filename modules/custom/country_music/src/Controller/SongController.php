fff8fb544a4b4e7268e9eebefa02f26dd3727a51
Ryan Weaver <ryan@thatsquality.com>
2018-04-08 18:14:39 -0400

    Step 4: Routing: Add Wildcard

--- modules/custom/country_music/src/Controller/SongController.php
+++ modules/custom/country_music/src/Controller/SongController.php
@@ -5,7 +5,7 @@ namespace Drupal\country_music\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class SongController {
-  public function writeSong() {
-    return new Response('I rode my truck, through some mud.');
+  public function writeSong($noun) {
+    return new Response(sprintf('I rode my %s, through some mud.', $noun));
   }
 }
