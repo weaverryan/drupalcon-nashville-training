3353050887d84cfca43d041e1cb986a7c0b49920
Ryan Weaver <ryan@thatsquality.com>
2018-04-08 18:14:41 -0400

    Step 6: Routing: Return render array

--- modules/custom/country_music/src/Controller/SongController.php
+++ modules/custom/country_music/src/Controller/SongController.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\country_music\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-
 class SongController {
   public function writeSong($noun) {
-    return new Response(sprintf('I rode my %s, through some mud.', $noun));
+    $title = sprintf('I rode my %s, through some mud.', $noun);
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $title,
+    ];
   }
 }
