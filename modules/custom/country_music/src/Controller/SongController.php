9b635d781c47c946a82cde4edbeb379321a06de8
Ryan Weaver <ryan@thatsquality.com>
2018-04-08 18:14:45 -0400

    Step 9: Service: Use config() controller shortcut

--- modules/custom/country_music/src/Controller/SongController.php
+++ modules/custom/country_music/src/Controller/SongController.php
@@ -2,8 +2,15 @@
 
 namespace Drupal\country_music\Controller;
 
-class SongController {
+use Drupal\Core\Controller\ControllerBase;
+
+class SongController extends ControllerBase {
   public function writeSong($noun) {
+    if ($noun === null) {
+      $noun = $this->config('country_music.default')
+        ->get('noun');
+    }
+
     $title = sprintf('I rode my %s, through some mud.', $noun);
 
     return [
