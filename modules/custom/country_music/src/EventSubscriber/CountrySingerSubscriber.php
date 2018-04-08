1000b1155336d7801c1ee66c4a4c0d0d13e7b445
Ryan Weaver <ryan@thatsquality.com>
2018-04-08 18:15:01 -0400

    Step 23: Event: EC add duration as header

--- modules/custom/country_music/src/EventSubscriber/CountrySingerSubscriber.php
+++ modules/custom/country_music/src/EventSubscriber/CountrySingerSubscriber.php
@@ -4,23 +4,36 @@ namespace Drupal\country_music\EventSubscriber;
 
 use Drupal\Core\Messenger\MessengerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class CountrySingerSubscriber implements EventSubscriberInterface {
 
   private $messenger;
 
+  private $startTime;
+
   public function __construct(MessengerInterface $messenger) {
     $this->messenger = $messenger;
   }
 
   public function onKernelRequest(GetResponseEvent $event) {
+    $this->startTime = microtime(true);
     $this->messenger->addStatus('Welcome to Music City USA');
+  }
+
+  public function onKernelResponse(FilterResponseEvent $event) {
+    $duration = microtime(true) - $this->startTime;
+
+    $event->getResponse()
+      ->headers
+      ->set('X-Song-Length', $duration * 1000);
   }
 
   public static function getSubscribedEvents() {
     return [
-      'kernel.request' => 'onKernelRequest'
+      'kernel.request' => 'onKernelRequest',
+      'kernel.response' => 'onKernelResponse',
     ];
   }
 }
