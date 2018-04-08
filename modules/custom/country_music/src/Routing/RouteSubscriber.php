8d70b810044e1bc3e5c6256efa88da0fae38e788
Ryan Weaver <ryan@thatsquality.com>
2018-04-08 18:15:02 -0400

    Step 24: Event: Generate route subscriber

---
+++ modules/custom/country_music/src/Routing/RouteSubscriber.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\country_music\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber.
+ *
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+  }
+}
