1b5534dcfbc3b3b9f67ab95b40a0f8fd3ecd0a5b
Ryan Weaver <ryan@thatsquality.com>
2018-04-08 18:14:44 -0400

    Step 8: Service: Generate config form

---
+++ modules/custom/country_music/src/Form/DefaultForm.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\country_music\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class DefaultForm.
+ */
+class DefaultForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'country_music.default',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'default_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('country_music.default');
+    $form['noun'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Noun'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#default_value' => $config->get('noun'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('country_music.default')
+      ->set('noun', $form_state->getValue('noun'))
+      ->save();
+  }
+
+}
